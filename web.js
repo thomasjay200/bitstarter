b462b80381c2ae562db7a3409cf42844b8b55d8c
thomasjay200 <benplummer@gmail.com>
2013-07-11 10:55:54 -0400

    Update web.js

--- web.js
+++ web.js
@@ -1,12 +1,6 @@
 var express = require('express');
 
-var fs = require('fs');
-
 var app = express.createServer(express.logger());
-
-var htmlfile = fs.readFile('index.html');
-
-var filebuffer = new Buffer(htmlfile);
 
 app.get('/', function(request, response) {
   response.send('htmlfile');
